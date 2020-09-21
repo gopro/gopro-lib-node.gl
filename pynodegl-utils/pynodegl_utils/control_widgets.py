NopeForge/nope.gl

commit 3baf8655446f98f3e9c1ef8cd401b5fdd89fb0cf

utils: remove PySide2 dependency from scene controllers
We don't want the scene() decorator to pull PySide2 in the dependency
tree, because this prevents using Python scenes on systems where
Qt/PySide2 are not well supported (this is typically the case on Windows
currently).

Note that the scene() decorator and associated objects probably belongs
into pynodegl as it is core to the Python API and relied on in the
ngl-tools.

pynodegl-utils/pynodegl_utils/control_widgets.py
@@ -188,3 +188,14 @@ class TextInput(_ControlWidget):
     @QtCore.Slot()
     def _submit_text(self):
         self.signal_change(self._text.toPlainText())
+
+
+control_to_widget = dict(
+    Range=Slider,
+    Vector=VectorWidget,
+    Color=ColorPicker,
+    Bool=Checkbox,
+    File=FilePicker,
+    List=ComboBox,
+    Text=TextInput,
+)

pynodegl-utils/pynodegl_utils/misc.py
@@ -27,10 +27,10 @@ import inspect
 import json
 import subprocess
 import pynodegl as ngl
-from pynodegl_utils import controls
+from collections import namedtuple
 
 
-def scene(**widgets_specs):
+def scene(**controls):
     def real_decorator(scene_func):
         def func_wrapper(idict=None, **extra_args):
             if idict is None:
@@ -45,20 +45,25 @@ def scene(**widgets_specs):
 
         final_specs = []
 
-        # Construct a arg -> default dict
+        # Construct widgets specs
+        widgets_specs = []
         func_specs = inspect.getfullargspec(scene_func)
         if func_specs.defaults:
             nb_optionnals = len(func_specs.defaults)
             for i, key in enumerate(func_specs.args[-nb_optionnals:]):
                 # Set controller defaults according to the function prototype
-                if key in widgets_specs:
-                    widgets_specs[key].set_default(func_specs.defaults[i])
+                control = controls.get(key)
+                if control is not None:
+                    default = func_specs.defaults[i]
+                    ctl_id = control.__class__.__name__
+                    ctl_data = control._asdict()
+                    widgets_specs.append((key, default, ctl_id, ctl_data))
 
-        # Transfers the widgets controls to the UI.
+        # Transfers the widget specs to the UI.
         # We could use the return value but it's better if the user can still
         # call its decorated scene function transparently inside his own code
         # without getting garbage along the return value.
-        func_wrapper.widgets_specs = list(widgets_specs.items())
+        func_wrapper.widgets_specs = widgets_specs
 
         # Flag the scene as a scene function so it's registered in the UI.
         func_wrapper.iam_a_ngl_scene_func = True
@@ -71,13 +76,13 @@ def scene(**widgets_specs):
     return real_decorator
 
 
-scene.Range = controls.Range
-scene.Vector = controls.Vector
-scene.Color = controls.Color
-scene.Bool = controls.Bool
-scene.File = controls.File
-scene.List = controls.List
-scene.Text = controls.Text
+scene.Range = namedtuple('Range',  'range unit_base', defaults=([0, 1], 1))
+scene.Vector = namedtuple('Vector', 'n minv maxv', defaults=(None, None))
+scene.Color = namedtuple('Color',  '')
+scene.Bool = namedtuple('Bool', '')
+scene.File = namedtuple('File', 'filter', defaults=(None,))
+scene.List = namedtuple('List', 'choices')
+scene.Text = namedtuple('Text', '')
 
 
 class Media:

pynodegl-utils/pynodegl_utils/controls.py
@@ -1,72 +1,0 @@
-#!/usr/bin/env python
-#
-# Copyright 2019 GoPro Inc.
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-
-from pynodegl_utils import control_widgets
-
-
-class _SceneControlWidget:
-
-    _widget_cls = None
-
-    def __init__(self, **kwargs):
-        self._widget_args = kwargs
-
-    def set_default(self, value):
-        self._value = value
-
-    def get_widget(self, name):
-        return self._widget_cls(name, self._value, **self._widget_args)
-
-
-class Range(_SceneControlWidget):
-
-    _widget_cls = control_widgets.Slider
-
-
-class Vector(_SceneControlWidget):
-
-    _widget_cls = control_widgets.VectorWidget
-
-
-class Color(_SceneControlWidget):
-
-    _widget_cls = control_widgets.ColorPicker
-
-
-class Bool(_SceneControlWidget):
-
-    _widget_cls = control_widgets.Checkbox
-
-
-class File(_SceneControlWidget):
-
-    _widget_cls = control_widgets.FilePicker
-
-
-class List(_SceneControlWidget):
-
-    _widget_cls = control_widgets.ComboBox
-
-
-class Text(_SceneControlWidget):
-
-    _widget_cls = control_widgets.TextInput

pynodegl-utils/pynodegl_utils/ui/toolbar.py
@@ -27,6 +27,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 import pynodegl as ngl
 
 from pynodegl_utils.config import Config
+from pynodegl_utils.control_widgets import control_to_widget
 
 
 class Toolbar(QtWidgets.QWidget):
@@ -182,8 +183,9 @@ class Toolbar(QtWidgets.QWidget):
 
     def _get_opts_widget_from_specs(self, widgets_specs):
         widgets = []
-        for name, controller in widgets_specs:
-            widget = controller.get_widget(name)
+        for key, default, ctl_id, ctl_data in widgets_specs:
+            widget_cls = control_to_widget[ctl_id]
+            widget = widget_cls(key, default, **ctl_data)
             widget.needSceneReload.connect(self._widget_scene_reload)
             widgets.append(widget)
         if not widgets:

gopro/gopro-lib-node.gl

commit 89ccec2344c312e522b57a83d48eb0079dc55fce

utils: rework control kwargs
In practice this also fixes a regression introduced in
3baf8655446f98f3e9c1ef8cd401b5fdd89fb0cf when using Vector(n=2),
typically in examples.medias:centered_medias, where the Qt widget has
minv and maxv in its kwargs, but they are set to None, so kwargs.get()
doesn't work as expected (no fallback on the default).

pynodegl-utils/pynodegl_utils/control_widgets.py
@@ -43,13 +43,11 @@ class _ControlWidget(QtWidgets.QWidget):
 
 class Slider(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value, range, unit_base):
         super().__init__(name)
         slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self._unit_base = kwargs.get('unit_base', 1)
-        if 'range' in kwargs:
-            srange = kwargs['range']
-            slider.setRange(srange[0] * self._unit_base, srange[1] * self._unit_base)
+        self._unit_base = unit_base
+        slider.setRange(range[0] * self._unit_base, range[1] * self._unit_base)
         slider.setValue(value * self._unit_base)
         self._label = QtWidgets.QLabel(self.get_label_text(value))
         slider.valueChanged.connect(self._slider_value_changed)
@@ -65,13 +63,12 @@ class Slider(_ControlWidget):
 
 class VectorWidget(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value, n, minv, maxv):
         super().__init__(name)
-        n = kwargs.get("n", 3)
         hlayout = QtWidgets.QHBoxLayout()
         self._spinboxes = []
-        elems_min = kwargs.get("minv", [0] * n)
-        elems_max = kwargs.get("maxv", [1] * n)
+        elems_min = minv if minv is not None else [0] * n
+        elems_max = maxv if maxv is not None else [1] * n
         assert 2 <= n <= 4
         assert len(elems_min) == len(elems_max) == len(value) == n
         for elem_value, elem_min, elem_max in zip(value, elems_min, elems_max):
@@ -94,7 +91,7 @@ class VectorWidget(_ControlWidget):
 
 class ColorPicker(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value):
         super().__init__(name)
         self._color_btn = QtWidgets.QPushButton()
         color = QtGui.QColor()
@@ -120,7 +117,7 @@ class ColorPicker(_ControlWidget):
 
 class Checkbox(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value):
         super().__init__(name)
         self._chkbox = QtWidgets.QCheckBox(name)
         self._chkbox.setChecked(value)
@@ -134,9 +131,9 @@ class Checkbox(_ControlWidget):
 
 class FilePicker(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value, filter):
         super().__init__(name)
-        self._filter = kwargs.get('filter', '')
+        self._filter = filter
         dialog_btn = QtWidgets.QPushButton('Open file')
         self._label = QtWidgets.QLabel(self.get_label_text(value))
         dialog_btn.pressed.connect(self._choose_filename)
@@ -154,11 +151,10 @@ class FilePicker(_ControlWidget):
 
 class ComboBox(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value, choices):
         super().__init__(name)
         combobox = QtWidgets.QComboBox()
         label = QtWidgets.QLabel(self.get_label_text())
-        choices = kwargs['choices']
         combobox.addItems(choices)
         combobox.setCurrentIndex(choices.index(value))
         combobox.currentTextChanged.connect(self._combobox_select)
@@ -172,7 +168,7 @@ class ComboBox(_ControlWidget):
 
 class TextInput(_ControlWidget):
 
-    def __init__(self, name, value, **kwargs):
+    def __init__(self, name, value):
         super().__init__(name)
         self._text = QtWidgets.QPlainTextEdit()
         self._text.setPlainText(value)

pynodegl-utils/pynodegl_utils/misc.py
@@ -80,7 +80,7 @@ scene.Range = namedtuple('Range',  'range unit_base', defaults=([0, 1], 1))
 scene.Vector = namedtuple('Vector', 'n minv maxv', defaults=(None, None))
 scene.Color = namedtuple('Color',  '')
 scene.Bool = namedtuple('Bool', '')
-scene.File = namedtuple('File', 'filter', defaults=(None,))
+scene.File = namedtuple('File', 'filter', defaults=('',))
 scene.List = namedtuple('List', 'choices')
 scene.Text = namedtuple('Text', '')
 
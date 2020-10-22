NopeForge/nope.gl

commit 7f8a6f888fb0e6f464a179f4c42c755b60da5656

rtt: move the rendertarget binding from rtt_draw() to ngli_pass_exec()
Previously we were binding (and clearing) the rendertarget before
drawing the subgraph. This leads to the folowing scenario (with
performance issues):

             (RTT 1)
               / \
              /   \
             /     \
         (RTT 2)  (Render)

Where we:

- started the renderpass of RTT 1 with a clear
- started the renderpass of RTT 2 with a clear
- resumed the renderpass of RTT 1 with a draw call, which has
  performance implications because the renderpass needs to reload
  the previous content of its attachments

This commit addresses this issue by binding/clearing at the right time.

The same scenario now perform the following steps:
- starts the renderpass of RTT 2
- starts the renderpass of RTT 1, clear attachments and perform a draw
  call

libnodegl/node_rtt.c
@@ -349,10 +349,6 @@ static void rtt_draw(struct ngl_node *node)
     struct gctx *gctx = ctx->gctx;
     struct rtt_priv *s = node->priv_data;
 
-    struct rendertarget *rt = s->rt;
-    struct rendertarget *prev_rt = ngli_gctx_get_rendertarget(gctx);
-    ngli_gctx_set_rendertarget(gctx, rt);
-
     int prev_vp[4] = {0};
     ngli_gctx_get_viewport(gctx, prev_vp);
 
@@ -365,20 +361,35 @@ static void rtt_draw(struct ngl_node *node)
         ngli_gctx_set_clear_color(gctx, s->clear_color);
     }
 
-    if (!(s->features & FEATURE_NO_CLEAR)) {
-        ngli_gctx_clear_color(gctx);
-        ngli_gctx_clear_depth_stencil(gctx);
-    }
+    struct rendertarget *prev_rendertarget = ctx->current_rendertarget;
+    int prev_clear = ctx->clear_current_rendertarget;
+
+    ctx->current_rendertarget = s->rt;
+    ctx->bind_current_rendertarget = 1;
+    ctx->clear_current_rendertarget = !(s->features & FEATURE_NO_CLEAR);
 
     ngli_node_draw(s->child);
 
+    if (ctx->bind_current_rendertarget) {
+        ngli_gctx_set_rendertarget(gctx, ctx->current_rendertarget);
+        if (ctx->clear_current_rendertarget) {
+            ngli_gctx_clear_color(gctx);
+            ngli_gctx_clear_depth_stencil(gctx);
+        }
+        ctx->bind_current_rendertarget = 0;
+        ctx->clear_current_rendertarget = 0;
+    }
+
     if (s->samples > 0)
-        ngli_rendertarget_resolve(rt);
+        ngli_rendertarget_resolve(ctx->current_rendertarget);
 
     if (s->invalidate_depth_stencil)
         ngli_gctx_invalidate_depth_stencil(gctx);
 
-    ngli_gctx_set_rendertarget(gctx, prev_rt);
+    ctx->current_rendertarget = prev_rendertarget;
+    ctx->bind_current_rendertarget = 1;
+    ctx->clear_current_rendertarget = prev_clear;
+
     ngli_gctx_set_viewport(gctx, prev_vp);
 
     if (s->use_clear_color)

libnodegl/gctx.c
@@ -74,6 +74,10 @@ int ngli_gctx_draw(struct gctx *s, struct ngl_node *scene, double t)
         goto end;
 
     if (scene) {
+        struct ngl_ctx *ctx = scene->ctx;
+        ctx->current_rendertarget = ngli_gctx_get_rendertarget(s);
+        ctx->bind_current_rendertarget = 0;
+        ctx->clear_current_rendertarget = 0;
         LOG(DEBUG, "draw scene %s @ t=%f", scene->label, t);
         ngli_node_draw(scene);
     }

libnodegl/nodes.h
@@ -83,6 +83,9 @@ struct ngl_ctx {
     struct rnode *rnode_pos;
     struct ngl_node *scene;
     struct ngl_config config;
+    struct rendertarget *current_rendertarget;
+    int bind_current_rendertarget;
+    int clear_current_rendertarget;
     struct darray modelview_matrix_stack;
     struct darray projection_matrix_stack;
     struct darray activitycheck_nodes;

libnodegl/pass.c
@@ -694,6 +694,17 @@ int ngli_pass_exec(struct pass *s)
     }
 
     if (s->pipeline_type == NGLI_PIPELINE_TYPE_GRAPHICS) {
+        if (ctx->bind_current_rendertarget) {
+            struct gctx *gctx = ctx->gctx;
+            ngli_gctx_set_rendertarget(gctx, ctx->current_rendertarget);
+            if (ctx->clear_current_rendertarget) {
+                ngli_gctx_clear_color(gctx);
+                ngli_gctx_clear_depth_stencil(gctx);
+            }
+            ctx->bind_current_rendertarget = 0;
+            ctx->clear_current_rendertarget = 0;
+        }
+
         if (s->indices_buffer)
             ngli_pipeline_draw_indexed(pipeline, s->indices_buffer, s->indices_format, s->nb_indices, s->nb_instances);
         else

gopro/gopro-lib-node.gl

commit c3b64c5bb2ac8aac4d21fd0f4ac310bbba0b080e

rtt: set the proper rendertarget at the end of rtt_draw()
If the render pass associated with the current rendertarget (LOAD_OP=CLEAR)
was not started, we must make this rendertarget current again at the end
of rtt_draw() otherwise the clear operation will never be executed.

The commit fixes the following scenario:

                                      (RTT 1)
                                         |
                                         |
                            .---------(Group)------.
                            |            |         |
                            |            |         |
                            |            |         |
                    (TimeRangeFilter) (RTT 2)  (Render 2)
                    /       |
                   /        |
      (TimeRangeNoop=0) (Render 1)

Where:
- the RTT 1 node detects that it needs to allocate two rendertargets,
  one with LOAD_OP=CLEAR and one with LOAD_OP=LOAD because there can be
  two separate render passes (one for each render)
- the RTT 1 node makes current the rendertarget with LOAD_OP=CLEAR
- the TimeRangeFilter node prevents the execution of Render 1 according
  to its time ranges preventing the start of the render pass associated
  with the current rendertarget
- the RTT 2 node sets its own rendertarget current
- the RTT 2 node calls ngli_draw_node()
- the RTT 2 node restores the previous rendertarget to the second one
  available (LOAD_OP=LOAD) which prevents the clear operation needed by
  RTT 1 to be executed

Fixes a regression introduced by
7f8a6f888fb0e6f464a179f4c42c755b60da5656.

libnodegl/node_rtt.c
@@ -410,8 +410,10 @@ static void rtt_draw(struct ngl_node *node)
         ctx->available_rendertargets[1],
     };
 
+    int current_rendertarget_index = 0;
     if (!ctx->begin_render_pass) {
         ngli_gctx_end_render_pass(gctx);
+        current_rendertarget_index = 1;
     }
 
     ctx->available_rendertargets[0] = s->available_rendertargets[0];
@@ -427,7 +429,7 @@ static void rtt_draw(struct ngl_node *node)
     }
     ngli_gctx_end_render_pass(gctx);
 
-    ctx->current_rendertarget = prev_rendertargets[1];
+    ctx->current_rendertarget = prev_rendertargets[current_rendertarget_index];
     ctx->available_rendertargets[0] = prev_rendertargets[0];
     ctx->available_rendertargets[1] = prev_rendertargets[1];
     ctx->begin_render_pass = 1;
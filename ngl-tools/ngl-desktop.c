NopeForge/nope.gl

commit e4e6fe0d842d83163250a34094f8f2bfab53ab13

tools: replace read/write on net fd with recv/send calls
read/write do not work with the incoming winsock2 support. The calls are
equivalent on *nix, with potentially just a difference on error codes.

ngl-tools/ngl-desktop.c
@@ -202,7 +202,7 @@ static int handle_tag_filepart(struct ctx *s, const uint8_t *data, int size)
         return ipc_pkt_add_rtag_fileend(s->send_pkt, s->upload_path);
     }
 
-    const ssize_t n = write(s->upload_fd, data, size);
+    const ssize_t n = send(s->upload_fd, data, size, 0);
     if (n < 0) {
         perror("write");
         close_upload_file(s);

ngl-tools/ngl-ipc.c
@@ -246,7 +246,7 @@ static int handle_response(struct ctx *s, const struct ipc_pkt *pkt)
     if (s->upload_fd > 0) {
         ipc_pkt_reset(s->send_pkt);
 
-        const ssize_t n = read(s->upload_fd, s->upload_buffer, UPLOAD_CHUNK_SIZE);
+        const ssize_t n = recv(s->upload_fd, s->upload_buffer, UPLOAD_CHUNK_SIZE, 0);
         if (n < 0)
             return NGL_ERROR_IO;
         int ret = ipc_pkt_add_qtag_filepart(s->send_pkt, s->upload_buffer, n);

ngl-tools/ipc.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
 
 #include <nodegl.h>
 
@@ -171,9 +172,9 @@ void ipc_pkt_freep(struct ipc_pkt **pktp)
 
 int ipc_send(int fd, const struct ipc_pkt *pkt)
 {
-    const ssize_t n = write(fd, pkt->data, pkt->size);
+    const ssize_t n = send(fd, pkt->data, pkt->size, 0);
     if (n < 0) {
-        perror("write");
+        perror("send");
         return NGL_ERROR_IO;
     }
     // XXX: should we loop instead?
@@ -188,11 +189,11 @@ static int readbuf(int fd, uint8_t *buf, ssize_t size)
 {
     ssize_t nr = 0;
     while (nr != size) {
-        const ssize_t n = read(fd, buf + nr, size - nr);
+        const ssize_t n = recv(fd, buf + nr, size - nr, 0);
         if (n == 0)
             return 0;
         if (n < 0) {
-            perror("read");
+            perror("recv");
             return NGL_ERROR_IO;
         }
         nr += n;

gopro/gopro-lib-node.gl

commit a0cc0a5e563bbfe4e9573599fba964e73cc99998

tools: use read/write instead of recv/send where appropriate
The recv and send functions must only be used on sockets (otherwise they
return ENOTSOCK).

Fixes a regression introduced by
e4e6fe0d842d83163250a34094f8f2bfab53ab13.

This regression has not been caught because these operations are used
for file transfers which are not used for local hooks.

ngl-tools/ngl-desktop.c
@@ -219,7 +219,7 @@ static int handle_tag_filepart(struct ctx *s, const uint8_t *data, int size)
         return ipc_pkt_add_rtag_fileend(s->send_pkt, s->upload_path);
     }
 
-    const ssize_t n = send(s->upload_fd, data, size, 0);
+    const ssize_t n = write(s->upload_fd, data, size);
     if (n < 0) {
         perror("write");
         close_upload_file(s);

ngl-tools/ngl-ipc.c
@@ -251,7 +251,7 @@ static int handle_response(struct ctx *s, const struct ipc_pkt *pkt)
     if (s->upload_fd > 0) {
         ipc_pkt_reset(s->send_pkt);
 
-        const ssize_t n = recv(s->upload_fd, s->upload_buffer, UPLOAD_CHUNK_SIZE, 0);
+        const ssize_t n = read(s->upload_fd, s->upload_buffer, UPLOAD_CHUNK_SIZE);
         if (n < 0)
             return NGL_ERROR_IO;
         int ret = ipc_pkt_add_qtag_filepart(s->send_pkt, s->upload_buffer, n);
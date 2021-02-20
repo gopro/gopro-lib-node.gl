NopeForge/nope.gl

commit 2e2acedf680a14e31adb02ad27e8e103c04b94aa

buffer: add and use explicit usage flags

libnodegl/node_text.c
@@ -36,6 +36,15 @@
 #include "topology.h"
 #include "utils.h"
 
+
+#define VERTEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_DYNAMIC_BIT      | \
+                            NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
+                            NGLI_BUFFER_USAGE_VERTEX_BUFFER_BIT) \
+
+#define INDEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_DYNAMIC_BIT      | \
+                           NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
+                           NGLI_BUFFER_USAGE_INDEX_BUFFER_BIT)  \
+
 struct pipeline_subdesc {
     struct pgcraft *crafter;
     struct pipeline *pipeline;
@@ -343,9 +352,9 @@ static int update_character_geometries(struct ngl_node *node)
             goto end;
         }
 
-        if ((ret = ngli_buffer_init(s->vertices, nb_vertices * sizeof(*vertices), NGLI_BUFFER_USAGE_DYNAMIC)) < 0 ||
-            (ret = ngli_buffer_init(s->uvcoords, nb_uvcoords * sizeof(*uvcoords), NGLI_BUFFER_USAGE_DYNAMIC)) < 0 ||
-            (ret = ngli_buffer_init(s->indices,  nb_indices  * sizeof(*indices),  NGLI_BUFFER_USAGE_DYNAMIC)) < 0)
+        if ((ret = ngli_buffer_init(s->vertices, nb_vertices * sizeof(*vertices), VERTEX_USAGE_FLAGS)) < 0 ||
+            (ret = ngli_buffer_init(s->uvcoords, nb_uvcoords * sizeof(*uvcoords), VERTEX_USAGE_FLAGS)) < 0 ||
+            (ret = ngli_buffer_init(s->indices,  nb_indices  * sizeof(*indices),  INDEX_USAGE_FLAGS)) < 0)
             goto end;
 
         struct pipeline_desc *descs = ngli_darray_data(&s->pipeline_descs);
@@ -392,8 +401,8 @@ static int init_bounding_box_geometry(struct ngl_node *node)
         return NGL_ERROR_MEMORY;
 
     int ret;
-    if ((ret = ngli_buffer_init(s->bg_vertices, sizeof(vertices), NGLI_BUFFER_USAGE_STATIC)) < 0 ||
-        (ret = ngli_buffer_init(s->bg_indices,  sizeof(indices),  NGLI_BUFFER_USAGE_STATIC)) < 0)
+    if ((ret = ngli_buffer_init(s->bg_vertices, sizeof(vertices), VERTEX_USAGE_FLAGS)) < 0 ||
+        (ret = ngli_buffer_init(s->bg_indices,  sizeof(indices),  INDEX_USAGE_FLAGS)) < 0)
         return ret;
 
     if ((ret = ngli_buffer_upload(s->bg_vertices, vertices, sizeof(vertices))) < 0 ||

libnodegl/backends/gl/buffer_gl.c
@@ -28,14 +28,11 @@
 #include "memory.h"
 #include "nodes.h"
 
-static const GLenum gl_usage_map[NGLI_BUFFER_USAGE_NB] = {
-    [NGLI_BUFFER_USAGE_STATIC]  = GL_STATIC_DRAW,
-    [NGLI_BUFFER_USAGE_DYNAMIC] = GL_DYNAMIC_DRAW,
-};
-
 static GLenum get_gl_usage(int usage)
 {
-    return gl_usage_map[usage];
+    if (usage & NGLI_BUFFER_USAGE_DYNAMIC_BIT)
+        return GL_DYNAMIC_DRAW;
+    return GL_STATIC_DRAW;
 }
 
 struct buffer *ngli_buffer_gl_create(struct gctx *gctx)

libnodegl/buffer.h
@@ -25,8 +25,13 @@
 struct gctx;
 
 enum {
-    NGLI_BUFFER_USAGE_STATIC,
-    NGLI_BUFFER_USAGE_DYNAMIC,
+    NGLI_BUFFER_USAGE_DYNAMIC_BIT        = 1 << 0,
+    NGLI_BUFFER_USAGE_TRANSFER_SRC_BIT   = 1 << 1,
+    NGLI_BUFFER_USAGE_TRANSFER_DST_BIT   = 1 << 2,
+    NGLI_BUFFER_USAGE_UNIFORM_BUFFER_BIT = 1 << 3,
+    NGLI_BUFFER_USAGE_STORAGE_BUFFER_BIT = 1 << 4,
+    NGLI_BUFFER_USAGE_INDEX_BUFFER_BIT   = 1 << 5,
+    NGLI_BUFFER_USAGE_VERTEX_BUFFER_BIT  = 1 << 6,
     NGLI_BUFFER_USAGE_NB
 };
 

libnodegl/hud.c
@@ -1205,7 +1205,10 @@ int ngli_hud_init(struct hud *s)
     if (!s->coords)
         return NGL_ERROR_MEMORY;
 
-    ret = ngli_buffer_init(s->coords, sizeof(coords), NGLI_BUFFER_USAGE_DYNAMIC);
+
+    ret = ngli_buffer_init(s->coords, sizeof(coords), NGLI_BUFFER_USAGE_DYNAMIC_BIT      |
+                                                      NGLI_BUFFER_USAGE_TRANSFER_DST_BIT |
+                                                      NGLI_BUFFER_USAGE_VERTEX_BUFFER_BIT);
     if (ret < 0)
         return ret;
 

libnodegl/hwconv.c
@@ -105,7 +105,8 @@ int ngli_hwconv_init(struct hwconv *hwconv, struct ngl_ctx *ctx,
     hwconv->vertices = ngli_buffer_create(gctx);
     if (!hwconv->vertices)
         return NGL_ERROR_MEMORY;
-    ret = ngli_buffer_init(hwconv->vertices, sizeof(vertices), NGLI_BUFFER_USAGE_STATIC);
+    ret = ngli_buffer_init(hwconv->vertices, sizeof(vertices), NGLI_BUFFER_USAGE_TRANSFER_DST_BIT |
+                                                               NGLI_BUFFER_USAGE_VERTEX_BUFFER_BIT);
     if (ret < 0)
         return ret;
 

libnodegl/node_animatedbuffer.c
@@ -71,7 +71,7 @@ static int animatedbuffer_init(struct ngl_node *node)
     struct buffer_priv *s = node->priv_data;
 
     s->dynamic = 1;
-    s->usage = NGLI_BUFFER_USAGE_DYNAMIC;
+    s->usage = NGLI_BUFFER_USAGE_DYNAMIC_BIT | NGLI_BUFFER_USAGE_TRANSFER_DST_BIT;
     s->data_comp = ngli_format_get_nb_comp(s->data_format);
     s->data_stride = ngli_format_get_bytes_per_pixel(s->data_format);
 

libnodegl/node_block.c
@@ -312,7 +312,7 @@ static int block_init(struct ngl_node *node)
 
     ngli_block_init(&s->block, s->layout);
 
-    s->usage = NGLI_BUFFER_USAGE_STATIC;
+    s->usage = NGLI_BUFFER_USAGE_TRANSFER_DST_BIT;
 
     for (int i = 0; i < s->nb_fields; i++) {
         const struct ngl_node *field_node = s->fields[i];
@@ -324,7 +324,7 @@ static int block_init(struct ngl_node *node)
             return ret;
 
         if (field_funcs[count ? IS_ARRAY : IS_SINGLE].has_changed(field_node))
-            s->usage = NGLI_BUFFER_USAGE_DYNAMIC;
+            s->usage = NGLI_BUFFER_USAGE_DYNAMIC_BIT;
 
         const struct block_field *fields = ngli_darray_data(&s->block.fields);
         const struct block_field *fi = &fields[i];

libnodegl/node_buffer.c
@@ -257,7 +257,7 @@ static int buffer_init(struct ngl_node *node)
         s->data_stride = ngli_format_get_bytes_per_pixel(s->data_format);
     }
 
-    s->usage = NGLI_BUFFER_USAGE_STATIC;
+    s->usage = NGLI_BUFFER_USAGE_TRANSFER_DST_BIT;
 
     if (s->data)
         return buffer_init_from_data(node);

gopro/gopro-lib-node.gl

commit a994231ba64072d38ef606d63d6797bddd93edad

text: only add dynamic flag to relevant buffers
Fixes a regression introduced by
2e2acedf680a14e31adb02ad27e8e103c04b94aa.

libnodegl/node_text.c
@@ -37,13 +37,14 @@
 #include "utils.h"
 
 
-#define VERTEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_DYNAMIC_BIT      | \
-                            NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
+#define VERTEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
                             NGLI_BUFFER_USAGE_VERTEX_BUFFER_BIT) \
 
-#define INDEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_DYNAMIC_BIT      | \
-                           NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
+#define INDEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_TRANSFER_DST_BIT | \
                            NGLI_BUFFER_USAGE_INDEX_BUFFER_BIT)  \
+
+#define DYNAMIC_VERTEX_USAGE_FLAGS (NGLI_BUFFER_USAGE_DYNAMIC_BIT | VERTEX_USAGE_FLAGS)
+#define DYNAMIC_INDEX_USAGE_FLAGS  (NGLI_BUFFER_USAGE_DYNAMIC_BIT | INDEX_USAGE_FLAGS)
 
 struct pipeline_subdesc {
     struct pgcraft *crafter;
@@ -352,9 +353,9 @@ static int update_character_geometries(struct ngl_node *node)
             goto end;
         }
 
-        if ((ret = ngli_buffer_init(s->vertices, nb_vertices * sizeof(*vertices), VERTEX_USAGE_FLAGS)) < 0 ||
-            (ret = ngli_buffer_init(s->uvcoords, nb_uvcoords * sizeof(*uvcoords), VERTEX_USAGE_FLAGS)) < 0 ||
-            (ret = ngli_buffer_init(s->indices,  nb_indices  * sizeof(*indices),  INDEX_USAGE_FLAGS)) < 0)
+        if ((ret = ngli_buffer_init(s->vertices, nb_vertices * sizeof(*vertices), DYNAMIC_VERTEX_USAGE_FLAGS)) < 0 ||
+            (ret = ngli_buffer_init(s->uvcoords, nb_uvcoords * sizeof(*uvcoords), DYNAMIC_VERTEX_USAGE_FLAGS)) < 0 ||
+            (ret = ngli_buffer_init(s->indices,  nb_indices  * sizeof(*indices),  DYNAMIC_INDEX_USAGE_FLAGS)) < 0)
             goto end;
 
         struct pipeline_desc *descs = ngli_darray_data(&s->pipeline_descs);
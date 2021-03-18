2413c2f3deeca6bee1ee14ba691637699215e7e3
Matthieu Bouron <matthieu.bouron@gmail.com>
2021-03-18 15:04:32 +0100

    WIP

--- build_scripts/windows/start_msvc_env.sh
+++ build_scripts/windows/start_msvc_env.sh
@@ -4,10 +4,7 @@ if [[ ! -v VCVARS64 ]]; then
   export VCVARS64='"'$(powershell.exe build_scripts/windows/find_vcvars64.ps1)'"'
 fi
 # Set Visual Studio environment variables
-cmd='WSLENV=VCVARS64/w cmd.exe /C %VCVARS64% \&\& wsl.exe'
-if [[ $# -ge 1 ]]; then
-  shift
-  cmd+=" $@"
-fi
+shift
+cmd="WSLENV=VCVARS64/w cmd.exe /C %VCVARS64% \&\& wsl.exe $@"
 echo "running command: $cmd"
 eval $cmd

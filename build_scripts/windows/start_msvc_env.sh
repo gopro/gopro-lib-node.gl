8cd8c181319b9715a3bb92035378b331125fa005
Matthieu Bouron <matthieu.bouron@gmail.com>
2021-03-18 17:57:00 +0100

    WIP

--- build_scripts/windows/start_msvc_env.sh
+++ build_scripts/windows/start_msvc_env.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
-set -x
-set -e
-echo "starting msvc environment"
+set -xe
 if [[ ! -v VCVARS64 ]]; then
     basedir=$(dirname "$0")
     export VCVARS64='"'$(powershell.exe $basedir/find_vcvars64.ps1)'"'
-    echo $VCVARS64
 fi
 # Set Visual Studio environment variables and launch wsl
-WSLENV=VCVARS64/w cmd.exe /C %VCVARS64% \&\& wsl.exe $@
+WSLENV=VCVARS64 cmd.exe /C %VCVARS64% \&\& wsl.exe $@

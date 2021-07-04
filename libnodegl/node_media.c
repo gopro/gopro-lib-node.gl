NopeForge/nope.gl

commit 27a93481858f729da54209819192a73d91ab9ecb

android: use AImageReader API if possible for MediaCodec interop

libnodegl/node_media.c
@@ -26,6 +26,7 @@
 
 #if defined(TARGET_ANDROID)
 #include <libavcodec/mediacodec.h>
+#include "android_imagereader.h"
 #endif
 
 #include "log.h"
@@ -148,6 +149,7 @@ static int media_init(struct ngl_node *node)
 
 #if defined(TARGET_ANDROID)
     struct ngl_ctx *ctx = node->ctx;
+    struct android_ctx *android_ctx = &ctx->android_ctx;
     const struct ngl_config *config = &ctx->config;
     struct gctx *gctx = ctx->gctx;
 
@@ -172,22 +174,34 @@ static int media_init(struct ngl_node *node)
         if (ret < 0)
             return ret;
 
-        s->android_handlerthread = ngli_android_handlerthread_new();
-        if (!s->android_handlerthread)
-            return NGL_ERROR_MEMORY;
+        void *android_surface = NULL;
+        if (android_ctx->has_native_imagereader_api) {
+            s->android_imagereader = ngli_android_imagereader_create(android_ctx, 1, 1,
+                                                                     NGLI_ANDROID_IMAGE_FORMAT_YUV_420_888, 2);
+            if (!s->android_imagereader)
+                return NGL_ERROR_MEMORY;
 
-        void *handler = ngli_android_handlerthread_get_native_handler(s->android_handlerthread);
-        if (!handler)
-            return NGL_ERROR_EXTERNAL;
+            ret = ngli_android_imagereader_get_window(s->android_imagereader, &android_surface);
+            if (ret < 0)
+                return ret;
+        } else {
+            s->android_handlerthread = ngli_android_handlerthread_new();
+            if (!s->android_handlerthread)
+                return NGL_ERROR_MEMORY;
 
-        struct texture_gl *texture_gl = (struct texture_gl *)s->android_texture;
-        s->android_surface = ngli_android_surface_new(texture_gl->id, handler);
-        if (!s->android_surface)
-            return NGL_ERROR_MEMORY;
+            void *handler = ngli_android_handlerthread_get_native_handler(s->android_handlerthread);
+            if (!handler)
+                return NGL_ERROR_EXTERNAL;
 
-        void *android_surface = ngli_android_surface_get_surface(s->android_surface);
-        if (!android_surface)
-            return NGL_ERROR_EXTERNAL;
+            struct texture_gl *texture_gl = (struct texture_gl *)s->android_texture;
+            s->android_surface = ngli_android_surface_new(texture_gl->id, handler);
+            if (!s->android_surface)
+                return NGL_ERROR_MEMORY;
+
+            void *android_surface = ngli_android_surface_get_surface(s->android_surface);
+            if (!android_surface)
+                return NGL_ERROR_EXTERNAL;
+        }
 
         sxplayer_set_option(s->player, "opaque", &android_surface);
     }
@@ -280,8 +294,14 @@ static void media_uninit(struct ngl_node *node)
     sxplayer_free(&s->player);
 
 #if defined(TARGET_ANDROID)
-    ngli_android_surface_free(&s->android_surface);
-    ngli_android_handlerthread_free(&s->android_handlerthread);
+    struct ngl_ctx *ctx = node->ctx;
+    struct android_ctx *android_ctx = &ctx->android_ctx;
+    if (android_ctx->has_native_imagereader_api) {
+        ngli_android_imagereader_freep(&s->android_imagereader);
+    } else {
+        ngli_android_surface_free(&s->android_surface);
+        ngli_android_handlerthread_free(&s->android_handlerthread);
+    }
     ngli_texture_freep(&s->android_texture);
 #endif
 }

libnodegl/Makefile
@@ -141,7 +141,7 @@ LIB_LDLIBS = -lm -lpthread
 
 LIB_EXTRA_OBJS_Linux     =
 LIB_EXTRA_OBJS_Darwin    =
-LIB_EXTRA_OBJS_Android   = jni_utils.o android_ctx.o android_utils.o android_looper.o android_surface.o android_handler.o android_handlerthread.o
+LIB_EXTRA_OBJS_Android   = jni_utils.o android_ctx.o android_utils.o android_looper.o android_surface.o android_handler.o android_handlerthread.o android_imagereader.o
 LIB_EXTRA_OBJS_iPhone    =
 LIB_EXTRA_OBJS_MinGW-w64 =
 

libnodegl/android_imagereader.c
@@ -1,0 +1,209 @@
+/*
+ * Copyright 2020 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <jni.h>
+#include <android/native_window.h>
+#include <media/NdkImageReader.h>
+
+#include "android_imagereader.h"
+#include "jni_utils.h"
+#include "log.h"
+#include "memory.h"
+
+struct android_image {
+    struct android_ctx *android_ctx;
+    AImage *image;
+};
+
+struct android_imagereader {
+    struct android_ctx *android_ctx;
+    AImageReader *reader;
+    jobject window;
+
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+    int buffer_available;
+};
+
+AHardwareBuffer *ngli_android_image_get_hardware_buffer(struct android_image *s)
+{
+    struct android_ctx *android_ctx = s->android_ctx;
+
+    AHardwareBuffer *hardware_buffer;
+    media_status_t status = android_ctx->AImage_getHardwareBuffer(s->image, &hardware_buffer);
+    if (status != AMEDIA_OK)
+        return NULL;
+    return hardware_buffer;
+}
+
+void ngli_android_image_freep(struct android_image **sp)
+{
+    if (!*sp)
+        return;
+
+    struct android_image *s = *sp;
+    struct android_ctx *android_ctx = s->android_ctx;
+    android_ctx->AImage_delete(s->image);
+
+    ngli_freep(sp);
+}
+
+static void on_buffer_available(void *context, AImageReader *reader)
+{
+    struct android_imagereader *s = context;
+    pthread_mutex_lock(&s->lock);
+    s->buffer_available = 1;
+    pthread_cond_signal(&s->cond);
+    pthread_mutex_unlock(&s->lock);
+}
+
+struct android_imagereader *ngli_android_imagereader_create(struct android_ctx *android_ctx, int width, int height, int format, int max_images)
+{
+    if (!android_ctx->has_native_imagereader_api)
+        return NULL;
+
+    struct android_imagereader *s = ngli_calloc(1, sizeof(*s));
+    if (!s)
+        return NULL;
+
+    s->android_ctx = android_ctx;
+    if (pthread_mutex_init(&s->lock, NULL)) {
+        ngli_freep(&s);
+        return NULL;
+    }
+
+    if (pthread_cond_init(&s->cond, NULL)) {
+        pthread_mutex_destroy(&s->lock);
+        ngli_freep(&s);
+        return NULL;
+    }
+
+    media_status_t status = android_ctx->AImageReader_new(width, height, format, max_images, &s->reader);
+    if (status != AMEDIA_OK) {
+        LOG(ERROR, "failed to allocate AImageReader");
+        goto fail;
+    }
+
+    AImageReader_ImageListener listener = {
+        .context = s,
+        .onImageAvailable = on_buffer_available,
+    };
+
+    status = android_ctx->AImageReader_setImageListener(s->reader, &listener);
+    if (status != AMEDIA_OK) {
+        LOG(ERROR, "failed to set image listener");
+        goto fail;
+    }
+
+    return s;
+fail:
+
+    pthread_mutex_destroy(&s->lock);
+    pthread_cond_destroy(&s->cond);
+    android_ctx->AImageReader_delete(s->reader);
+    ngli_freep(&s);
+
+    return NULL;
+}
+
+int ngli_android_imagereader_get_window(struct android_imagereader *s, void **window)
+{
+    struct android_ctx *android_ctx = s->android_ctx;
+
+    if (s->window) {
+        *window = s->window;
+        return 0;
+    }
+
+    ANativeWindow *native_window;
+    media_status_t status = android_ctx->AImageReader_getWindow(s->reader, &native_window);
+    if (status != AMEDIA_OK)
+        return NGL_ERROR_EXTERNAL;
+
+    JNIEnv *env = ngli_jni_get_env();
+    if (!env)
+        return NGL_ERROR_EXTERNAL;
+
+    jobject object = android_ctx->ANativeWindow_toSurface(env, native_window);
+    s->window = (*env)->NewGlobalRef(env, object);
+    (*env)->DeleteLocalRef(env, object);
+
+    *window = s->window;
+    return *window ? 0 : NGL_ERROR_EXTERNAL;
+}
+
+int ngli_android_imagereader_acquire_next_image(struct android_imagereader *s, struct android_image **imagep)
+{
+    struct android_ctx *android_ctx = s->android_ctx;
+
+    AImage *android_image;
+    media_status_t status;
+    int nb_retries = 0;
+
+    pthread_mutex_lock(&s->lock);
+    for (;;) {
+        status = android_ctx->AImageReader_acquireNextImage(s->reader, &android_image);
+        if (!nb_retries++ && status == AMEDIA_IMGREADER_NO_BUFFER_AVAILABLE) {
+            struct timespec ts;
+            clock_gettime(CLOCK_REALTIME, &ts);
+            ts.tv_sec += 1;
+            pthread_cond_timedwait(&s->cond, &s->lock, &ts);
+        } else {
+            break;
+        }
+    }
+    pthread_mutex_unlock(&s->lock);
+
+    if (status != AMEDIA_OK)
+        return NGL_ERROR_EXTERNAL;
+
+    struct android_image *image = ngli_calloc(1, sizeof(*image));
+    if (!image) {
+        android_ctx->AImage_delete(android_image);
+        return NGL_ERROR_MEMORY;
+    }
+    image->android_ctx = android_ctx;
+    image->image = android_image;
+    *imagep = image;
+
+    return 0;
+}
+
+void ngli_android_imagereader_freep(struct android_imagereader **sp)
+{
+    if (!*sp)
+        return;
+
+    struct android_imagereader *s = *sp;
+    struct android_ctx *android_ctx = s->android_ctx;
+    android_ctx->AImageReader_delete(s->reader);
+
+    pthread_mutex_destroy(&s->lock);
+    pthread_cond_destroy(&s->cond);
+
+    JNIEnv *env = ngli_jni_get_env();
+    if (env)
+        (*env)->DeleteGlobalRef(env, s->window);
+
+    ngli_freep(sp);
+}

libnodegl/android_imagereader.h
@@ -1,0 +1,62 @@
+/*
+ * Copyright 2019 GoPro Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef ANDROID_IMAGEREADER_H
+#define ANDROID_IMAGEREADER_H
+
+#include <stdint.h>
+#include <android/hardware_buffer.h>
+
+#include "android_ctx.h"
+
+enum {
+    NGLI_ANDROID_IMAGE_FORMAT_RGBA_8888         = 0x1,
+    NGLI_ANDROID_IMAGE_FORMAT_RGBX_8888         = 0x2,
+    NGLI_ANDROID_IMAGE_FORMAT_RGB_888           = 0x3,
+    NGLI_ANDROID_IMAGE_FORMAT_RGB_565           = 0x4,
+    NGLI_ANDROID_IMAGE_FORMAT_RGBA_FP16         = 0x16,
+    NGLI_ANDROID_IMAGE_FORMAT_YUV_420_888       = 0x23,
+    NGLI_ANDROID_IMAGE_FORMAT_JPEG              = 0x100,
+    NGLI_ANDROID_IMAGE_FORMAT_RAW16             = 0x20,
+    NGLI_ANDROID_IMAGE_FORMAT_RAW_PRIVATE       = 0x24,
+    NGLI_ANDROID_IMAGE_FORMAT_RAW10             = 0x25,
+    NGLI_ANDROID_IMAGE_FORMAT_RAW12             = 0x26,
+    NGLI_ANDROID_IMAGE_FORMAT_DEPTH16           = 0x44363159,
+    NGLI_ANDROID_IMAGE_FORMAT_DEPTH_POINT_CLOUD = 0x101,
+    NGLI_ANDROID_IMAGE_FORMAT_PRIVATE           = 0x22,
+    NGLI_ANDROID_IMAGE_FORMAT_Y8                = 0x20203859,
+    NGLI_ANDROID_IMAGE_FORMAT_HEIC              = 0x48454946,
+    NGLI_ANDROID_IMAGE_FORMAT_DEPTH_JPEG        = 0x69656963
+};
+
+struct android_image;
+
+AHardwareBuffer *ngli_android_image_get_hardware_buffer(struct android_image *s);
+void ngli_android_image_freep(struct android_image **sp);
+
+struct android_imagereader;
+
+struct android_imagereader *ngli_android_imagereader_create(struct android_ctx *api, int width, int height, int format, int max_images);
+int  ngli_android_imagereader_get_window(struct android_imagereader *s, void **window);
+int  ngli_android_imagereader_acquire_next_image(struct android_imagereader *s, struct android_image **imagep);
+void ngli_android_imagereader_freep(struct android_imagereader **sp);
+
+#endif /* ANDROID_IMAGEREADER_H */

libnodegl/hwupload_mediacodec_gl.c
@@ -23,10 +23,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <sxplayer.h>
-
 #include <libavcodec/mediacodec.h>
+#include <android/hardware_buffer_jni.h>
 
+#include "android_imagereader.h"
 #include "android_surface.h"
+#include "egl.h"
 #include "format.h"
 #include "gctx_gl.h"
 #include "glincludes.h"
@@ -37,6 +39,11 @@
 #include "nodegl.h"
 #include "nodes.h"
 #include "texture_gl.h"
+
+struct hwupload_mc {
+    struct android_image *android_image;
+    EGLImageKHR egl_image;
+};
 
 static int support_direct_rendering(struct ngl_node *node)
 {
@@ -94,7 +101,7 @@ static int mc_init(struct ngl_node *node, struct sxplayer_frame *frame)
     return 0;
 }
 
-static int mc_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
+static int mc_map_frame_surfacetexture(struct ngl_node *node, struct sxplayer_frame *frame)
 {
     struct texture_priv *s = node->priv_data;
     struct hwupload *hwupload = &s->hwupload;
@@ -117,8 +124,95 @@ static int mc_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
     return 0;
 }
 
+static int mc_map_frame_imagereader(struct ngl_node *node, struct sxplayer_frame *frame)
+{
+    struct texture_priv *s = node->priv_data;
+    struct hwupload *hwupload = &s->hwupload;
+    struct hwupload_mc *mc = hwupload->hwmap_priv_data;
+
+    struct ngl_ctx *ctx = node->ctx;
+    struct gctx_gl *gctx_gl = (struct gctx_gl *)ctx->gctx;
+    struct glcontext *gl = gctx_gl->glcontext;
+    struct media_priv *media = s->data_src->priv_data;
+
+    AVMediaCodecBuffer *buffer = (AVMediaCodecBuffer *)frame->data;
+    int ret = av_mediacodec_release_buffer(buffer, 1);
+    if (ret < 0)
+        return ret;
+
+    struct android_image *android_image;
+    ret = ngli_android_imagereader_acquire_next_image(media->android_imagereader, &android_image);
+    if (ret < 0)
+        return ret;
+
+    ngli_eglDestroyImageKHR(gl, mc->egl_image);
+    mc->egl_image = NULL;
+    ngli_android_image_freep(&mc->android_image);
+
+    mc->android_image = android_image;
+
+    AHardwareBuffer *hardware_buffer = ngli_android_image_get_hardware_buffer(mc->android_image);
+    if (!hardware_buffer)
+        return NGL_ERROR_EXTERNAL;
+
+    EGLClientBuffer egl_buffer = ngli_eglGetNativeClientBufferANDROID(gl, hardware_buffer);
+    if (!egl_buffer)
+        return NGL_ERROR_EXTERNAL;
+
+    static const EGLint attrs[] = {
+        EGL_IMAGE_PRESERVED_KHR,
+        EGL_TRUE,
+        EGL_NONE,
+    };
+
+    const struct texture_gl *texture_gl = (struct texture_gl *)media->android_texture;
+    const GLuint id = texture_gl->id;
+
+    mc->egl_image = ngli_eglCreateImageKHR(gl, EGL_NO_CONTEXT, EGL_NATIVE_BUFFER_ANDROID, egl_buffer, attrs);
+    if (!mc->egl_image) {
+        LOG(ERROR, "failed to create egl image");
+        return NGL_ERROR_EXTERNAL;
+    }
+
+    ngli_glBindTexture(gl, GL_TEXTURE_EXTERNAL_OES, id);
+    ngli_glEGLImageTargetTexture2DOES(gl, GL_TEXTURE_EXTERNAL_OES, mc->egl_image);
+
+    ngli_texture_gl_set_dimensions(media->android_texture, frame->width, frame->height, 0);
+
+    return 0;
+}
+
+static int mc_map_frame(struct ngl_node *node, struct sxplayer_frame *frame)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct android_ctx *android_ctx = &ctx->android_ctx;
+
+    if (android_ctx->has_native_imagereader_api)
+        return mc_map_frame_imagereader(node, frame);
+
+    return mc_map_frame_surfacetexture(node, frame);
+}
+
+static void mc_uninit(struct ngl_node *node)
+{
+    struct ngl_ctx *ctx = node->ctx;
+    struct android_ctx *android_ctx = &ctx->android_ctx;
+    struct gctx_gl *gctx_gl = (struct gctx_gl *)ctx->gctx;
+    struct glcontext *gl = gctx_gl->glcontext;
+    struct texture_priv *s = node->priv_data;
+    struct hwupload *hwupload = &s->hwupload;
+    struct hwupload_mc *mc = hwupload->hwmap_priv_data;
+
+    if (android_ctx->has_native_imagereader_api) {
+        ngli_eglDestroyImageKHR(gl, mc->egl_image);
+        ngli_android_image_freep(&mc->android_image);
+    }
+}
+
 const struct hwmap_class ngli_hwmap_mc_gl_class = {
     .name      = "mediacodec (oes zero-copy)",
+    .priv_size = sizeof(struct hwupload_mc),
     .init      = mc_init,
     .map_frame = mc_map_frame,
+    .uninit    = mc_uninit,
 };

libnodegl/nodes.h
@@ -42,6 +42,7 @@
 #include "android_ctx.h"
 #include "android_handlerthread.h"
 #include "android_surface.h"
+#include "android_imagereader.h"
 #endif
 
 #if defined(TARGET_IPHONE)
@@ -315,6 +316,7 @@ struct media_priv {
     struct texture *android_texture;
     struct android_surface *android_surface;
     struct android_handlerthread *android_handlerthread;
+    struct android_imagereader *android_imagereader;
 #endif
 };
 

gopro/gopro-lib-node.gl

commit 6e871ff469c2e374e049f8ab44065ae9388fde96

media: fix shadowing of the android_texture variable
Fixes shadowing of the android_texture variable which prevented the
MediaCodec decoder to output to a surface on devices that do not
support the AImageReader API.

Fixes a regression introduced in
27a93481858f729da54209819192a73d91ab9ecb.

libnodegl/node_media.c
@@ -216,7 +216,7 @@ static int media_init(struct ngl_node *node)
             if (!s->android_surface)
                 return NGL_ERROR_MEMORY;
 
-            void *android_surface = ngli_android_surface_get_surface(s->android_surface);
+            android_surface = ngli_android_surface_get_surface(s->android_surface);
             if (!android_surface)
                 return NGL_ERROR_EXTERNAL;
         }